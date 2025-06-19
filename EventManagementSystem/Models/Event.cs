b92a7f9dadd4cfe6e344bebb7c312a15ce26a505
prabhuti-naner <prabhuti.nanera@clomotech.com>
2025-06-19 20:58:17 +0530

    Enhance routing and navigation

--- EventManagementSystem/Models/Event.cs
+++ EventManagementSystem/Models/Event.cs
@@ -6,17 +6,12 @@ namespace EventManagementSystem.Models
     public class Event
     {
         public int Id { get; set; }
-        [Required]
         public string Title { get; set; }
-        public string Description { get; set; }
-        [Required]
         public DateTime Date { get; set; }
-        [Required]
         public string Location { get; set; }
-        [Required]
-        [Range(1, int.MaxValue)]
-        public int MaxTickets { get; set; }
         public bool IsApproved { get; set; }
+        public int MaxTickets { get; set; }
         public string OrganizerId { get; set; }
+        public string Description { get; set; }
     }
 }
